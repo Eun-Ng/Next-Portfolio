abff6986509e79f1e31c5ff35f945227b11d362c
Eun-Ng <dhkdldms@gmail.com>
2022-08-31 23:26:22 +0900

    Fix open graph

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,6 @@ export default function Home() {
     <Layout>
       <Head>
         <title>남궁은 포트폴리오</title>
-        <meta property='og:type' content='website' />
-        <meta property='og:site_name' content='남궁은 포트폴리오' />
-        <meta property='og:title' content='남궁은 포트폴리오' />
-        <meta
-          property='og:description'
-          content='Next.j로 만든 포트폴리오 사이트입니다..'
-        />
-        <meta property='og:image' content='../public/portfolio_main_img.png' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <section className='flex min-h-screen flex-col items-center justify-center text-gray-600 body-font'>
