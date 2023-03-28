873c1b2ff0928f7d4129fee41c7182bf247a85d1
Eun-Ng <dhkdldms@gmail.com>
2023-03-28 23:52:48 +0900

    fix: landing main contents

--- components/home/MainContents.js
+++ components/home/MainContents.js
@@ -6,7 +6,7 @@ const MainContents = () => {
     <>
       <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
         <h2 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
-          안녕하세요 0년차 프론트엔드 개발자 남궁은입니다.
+          안녕하세요. 프론트엔드 개발자 남궁은입니다!
         </h2>
 
         <p className='mb-8 leading-relaxed text-xl'>
